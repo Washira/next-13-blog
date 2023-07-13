4436ed39b5cc7898708a2dd9ef7e42b460c59ca7
Washira <us_pou@hotmail.com>
2023-07-13 22:57:35 +0700

    styles of img social

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -6,8 +6,11 @@ const Footer = () => {
   return (
     <div className={styles.container}>
       <div>©2023 Toupawa. All rights reserved.</div>
-      <div>
-        <Image src="/1.png" width={15} height={15} alt="toupawa" />
+      <div className={styles.social}>
+        <Image src="/1.png" width={15} height={15} className={styles.icon} alt="toupawa" />
+        <Image src="/2.png" width={15} height={15} className={styles.icon} alt="toupawa" />
+        <Image src="/3.png" width={15} height={15} className={styles.icon} alt="toupawa" />
+        <Image src="/4.png" width={15} height={15} className={styles.icon} alt="toupawa" />
       </div>
     </div>
   )
