8dd8e1a727e37ae63454a76fd54a919cc7bcb6b2
Washira <us_pou@hotmail.com>
2023-07-16 10:42:26 +0700

    Fix home page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,8 +1,18 @@
 import Image from 'next/image'
-// import styles from './page.module.css'
+import styles from './page.module.css'
+import Hero from 'public/hero.png'
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <h1 className={styles.title}>Toupawa Website</h1>
+        <p className={styles.desc}>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas vitae est eius inventore ipsa labore temporibus nostrum nesciunt deserunt molestiae! Sapiente nobis quis doloribus facilis! Illo ullam distinctio nesciunt aspernatur.</p>
+        <button className={styles.button}>Click</button>
+      </div>
+      <div className={styles.item}>
+        <Image src={Hero} alt='' className={styles.img} />
+      </div>
+    </div>
   )
 }
